56494ea95be9ad69cd6ea5b8ac7120bbdb4823a0
Ayush Garg <aayushgarg152@gmail.com>
2021-09-30 23:46:46 +0530

    Added Navbar Changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from "./nav"
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 export default function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <Navbar />
+      </Router>
+      
+    </div>
+  );
+
+};
---
+++ src/screens/docs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Docs = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <h1>Docs</h1>
+    </div>
+  );
+};
+
+export default Docs;
